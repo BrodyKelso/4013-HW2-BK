734ccacc86a97c14eecee30d80284b3d2565162e
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-09-17 20:43:06 -0500

    doing php function

--- post-result.php
+++ post-result.php
@@ -1,20 +1,26 @@
 <?php
-$pageTitle = "Post result";
-include "view-header.php";
+
+function PostDisplay() {
+    $pageTitle = "Post result";
+    include "view-header.php";
 ?>
     <h1>Post result</h1>
 
 <?php
-if (isset($_POST["my-name"])) {
+    if (isset($_POST["my-name"])) {
 ?>
- <p> The value sent is:</p>
+        <p> The value sent is:</p>
 <?php
-    echo $_POST["my-name"];
-} else {
+        echo $_POST["my-name"];
+    } else {
 ?>
-  <p> Nothing was posted to the page.</p>
+        <p> Nothing was posted to the page.</p>
 <?php
+    }
+
+    include "view-footer.php";
 }
 
-include "view-footer.php";
+PostDisplay();
+
 ?>
